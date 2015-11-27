f1a01c54a11cc4698a1ea09bebab654e547d2884
BW <bw@localhost.localdomain>
2015-11-27 20:18:56 +0900

    sudoku

--- Practice/Practice/Practice.cpp
+++ Practice/Practice/Practice.cpp
@@ -2,23 +2,85 @@
 // Created by bw on 15. 11. 9.
 //
 
-#include <iostream>
+#include <stdio.h>
 
-using namespace std;
+int isAvailable(int puzzle[][9], int row, int col, int num) {
+    int rowStart = (row / 3) * 3;
+    int colStart = (col / 3) * 3;
+    int i, j;
+
+    for (i = 0; i < 9; ++i) {
+        if (puzzle[row][i] == num)
+            return 0;
+        if (puzzle[i][col] == num)
+            return 0;
+        if (puzzle[rowStart + (i % 3)][colStart + (i / 3)] == num)
+            return 0;
+    }
+    return 1;
+}
+
+int fillSudoku(int puzzle[][9], int row, int col) {
+    int i;
+    if (row < 9 && col < 9) {
+        if (puzzle[row][col] != 0) {
+            if ((col + 1) < 9)
+                return fillSudoku(puzzle, row, col + 1);
+            else if ((row + 1) < 9)
+                return fillSudoku(puzzle, row + 1, 0);
+            else
+                return 1;
+        }
+        else {
+            for (i = 0; i < 9; ++i) {
+                if (isAvailable(puzzle, row, col, i + 1)) {
+                    puzzle[row][col] = i + 1;
+                    if ((col + 1) < 9) {
+                        if (fillSudoku(puzzle, row, col + 1))
+                            return 1;
+                        else
+                            puzzle[row][col] = 0;
+                    }
+                    else if ((row + 1) < 9) {
+                        if (fillSudoku(puzzle, row + 1, 0))
+                            return 1;
+                        else
+                            puzzle[row][col] = 0;
+                    }
+                    else
+                        return 1;
+                }
+            }
+        }
+        return 0;
+    }
+    else return 1;
+}
 
 int main() {
-    int card[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    int i, j, testCase;
+    int puzzle[9][9];
 
-    for (int i = 0; i < 10; i++) {
-        int s, e;
-        cin >> s >> e;
-        for (;s < e;s++, e--) {
-            int temp = card[s];
-            card[s] = card[e];
-            card[e] = temp;
+    freopen("input.txt", "r", stdin);
+
+    scanf("%d", &testCase);
+
+    while (testCase--) {
+        for (i = 0; i < 9; i++) {
+            for (j = 0; j < 9; j++)
+                scanf("%d", &puzzle[i][j]);
         }
+
+        if (fillSudoku(puzzle, 0, 0)) {
+            for (i = 1; i < 10; ++i) {
+                for (j = 1; j < 10; ++j)
+                    printf("%d ", puzzle[i - 1][j - 1]);
+                printf("\n");
+            }
+        }
+        else
+            printf("NO SOLUTION");
     }
-    for (int i = 1; i < 21; i++)
-        cout << card[i] << " ";
-    cout << endl;
+
+    return 0;
 }
--- Practice/Practice/temp.cpp
+++ Practice/Practice/temp.cpp
@@ -2,58 +2,23 @@
 // Created by bw on 15. 10. 25.
 //
 
-/*************************************************************************
-* *Problem Palindrome
-* 국민대학교 전자정보통신대학 컴퓨터공학부 3 학년 *
-* 20113310 이 웅 *
-* *
-*************************************************************************/
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <string.h>
 
 using namespace std;
-char strSol[10000] = "";
-int cnt = 0;
 
-void con(int n, int base) {
-    string str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@?";
-    if (n >= base)
-        con(n / base, base);
-    strSol[cnt++] = str[n % base];
-}
+int main() {
+    int card[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
-bool pal(int start, int end) {
-    if (start > end)
-        return true;
-    else if (strSol[start] == strSol[end])
-        return pal(++start, --end);
-    else if (strSol[start] != strSol[end])
-        return false;
-}
-
-int main(void) {
-    ifstream in;
-    in.open("input.txt");
-    int testcases;
-    in >> testcases;
-    int value;
-    for (int i = 0; i < testcases; i++) {
-        in >> value;
-        for (int j = 2; j <= 64; j++) {
-            con(value, j);
-            cout << strSol << endl;
-            if (pal(0, cnt - 1)) {
-                cout << 1 << endl;
-                break;
-            }
-            else if (j == 64) {
-                cout << 0 << endl;
-                break;
-            }
-            memset(strSol, 0, sizeof(char) * 10000);
-            cnt = 0;
+    for (int i = 0; i < 10; i++) {
+        int s, e;
+        cin >> s >> e;
+        for (;s < e;s++, e--) {
+            int temp = card[s];
+            card[s] = card[e];
+            card[e] = temp;
         }
     }
+    for (int i = 1; i < 21; i++)
+        cout << card[i] << " ";
+    cout << endl;
 }
