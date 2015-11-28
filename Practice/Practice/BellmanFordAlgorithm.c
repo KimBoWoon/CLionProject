f2e41c2389cf86e012c349786e744e5280a147e6
KimBoWoon <haioer@naver.com>
2015-11-29 00:15:31 +0900

    Dancing Links Sudoku

--- Practice/Practice/BellmanFordAlgorithm.c
+++ Practice/Practice/BellmanFordAlgorithm.c
@@ -1,11 +1,12 @@
 //
 // Created by bw on 15. 11. 26.
 //
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 
 typedef struct {
-    int u, v, w;
+	int u, v, w;
 } Edge;
 
 int n;
@@ -19,59 +20,59 @@ int d[32]; /* d[i] is the minimum distance from node s to node i */
 #define INFINITY 10000
 
 void printDist() {
-    int i;
+	int i;
 
-    printf("Distances:\n");
+	printf("Distances:\n");
 
-    for (i = 0; i < n; ++i)
-        printf("to %d\t", i + 1);
-    printf("\n");
+	for (i = 0; i < n; ++i)
+		printf("to %d\t", i + 1);
+	printf("\n");
 
-    for (i = 0; i < n; ++i)
-        printf("%d\t\t", d[i]);
+	for (i = 0; i < n; ++i)
+		printf("%d\t", d[i]);
 
-    printf("\n");
+	printf("\n");
 }
 
 void bellman_ford(int s) {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < n; ++i)
-        d[i] = INFINITY;
+	for (i = 0; i < n; ++i)
+		d[i] = INFINITY;
 
-    d[s] = 0;
+	d[s] = 0;
 
-    for (i = 0; i < n - 1; ++i)
-        for (j = 0; j < e; ++j)
-            if (d[edges[j].u] + edges[j].w < d[edges[j].v])
-                d[edges[j].v] = d[edges[j].u] + edges[j].w;
+	for (i = 0; i < n - 1; ++i)
+		for (j = 0; j < e; ++j)
+			if (d[edges[j].u] + edges[j].w < d[edges[j].v])
+				d[edges[j].v] = d[edges[j].u] + edges[j].w;
 }
 
 int main(int argc, char *argv[]) {
-    int i, j;
-    int w;
+	int i, j;
+	int w;
 
-    FILE *fin = fopen("BellmanFordInput.txt", "r");
-    fscanf(fin, "%d", &n);
-    e = 0;
+	FILE *fin = fopen("BellmanFordInput.txt", "r");
+	fscanf(fin, "%d", &n);
+	e = 0;
 
-    for (i = 0; i < n; ++i)
-        for (j = 0; j < n; ++j) {
-            fscanf(fin, "%d", &w);
-            if (w != 0) {
-                edges[e].u = i;
-                edges[e].v = j;
-                edges[e].w = w;
-                ++e;
-            }
-        }
-    fclose(fin);
+	for (i = 0; i < n; ++i)
+		for (j = 0; j < n; ++j) {
+			fscanf(fin, "%d", &w);
+			if (w != 0) {
+				edges[e].u = i;
+				edges[e].v = j;
+				edges[e].w = w;
+				++e;
+			}
+		}
+	fclose(fin);
 
-//    printDist();
+	//    printDist();
 
-    bellman_ford(0);
+	bellman_ford(0);
 
-    printDist();
+	printDist();
 
-    return 0;
+	return 0;
 }
--- Practice/Practice/Sudoku.cpp
+++ Practice/Practice/Sudoku.cpp
@@ -3,84 +3,95 @@
 //
 
 #include <stdio.h>
+//#include <Windows.h>
+
+#pragma warning(disable:4996)
 
 int isAvailable(int puzzle[][9], int row, int col, int num) {
-    int rowStart = (row / 3) * 3;
-    int colStart = (col / 3) * 3;
-    int i, j;
+	int rowStart = (row / 3) * 3;
+	int colStart = (col / 3) * 3;
+	int i, j;
 
-    for (i = 0; i < 9; ++i) {
-        if (puzzle[row][i] == num)
-            return 0;
-        if (puzzle[i][col] == num)
-            return 0;
-        if (puzzle[rowStart + (i % 3)][colStart + (i / 3)] == num)
-            return 0;
-    }
-    return 1;
+	for (i = 0; i < 9; ++i) {
+		if (puzzle[row][i] == num)
+			return 0;
+		if (puzzle[i][col] == num)
+			return 0;
+		if (puzzle[rowStart + (i % 3)][colStart + (i / 3)] == num)
+			return 0;
+	}
+	return 1;
 }
 
 int fillSudoku(int puzzle[][9], int row, int col) {
-    int i;
-    if (row < 9 && col < 9) {
-        if (puzzle[row][col] != 0) {
-            if ((col + 1) < 9)
-                return fillSudoku(puzzle, row, col + 1);
-            else if ((row + 1) < 9)
-                return fillSudoku(puzzle, row + 1, 0);
-            else
-                return 1;
-        }
-        else {
-            for (i = 0; i < 9; ++i) {
-                if (isAvailable(puzzle, row, col, i + 1)) {
-                    puzzle[row][col] = i + 1;
-                    if ((col + 1) < 9) {
-                        if (fillSudoku(puzzle, row, col + 1))
-                            return 1;
-                        else
-                            puzzle[row][col] = 0;
-                    }
-                    else if ((row + 1) < 9) {
-                        if (fillSudoku(puzzle, row + 1, 0))
-                            return 1;
-                        else
-                            puzzle[row][col] = 0;
-                    }
-                    else
-                        return 1;
-                }
-            }
-        }
-        return 0;
-    }
-    else return 1;
+	int i;
+	if (row < 9 && col < 9) {
+		if (puzzle[row][col] != 0) {
+			if ((col + 1) < 9)
+				return fillSudoku(puzzle, row, col + 1);
+			else if ((row + 1) < 9)
+				return fillSudoku(puzzle, row + 1, 0);
+			else
+				return 1;
+		}
+		else {
+			for (i = 0; i < 9; ++i) {
+				if (isAvailable(puzzle, row, col, i + 1)) {
+					puzzle[row][col] = i + 1;
+					if ((col + 1) < 9) {
+						if (fillSudoku(puzzle, row, col + 1))
+							return 1;
+						else
+							puzzle[row][col] = 0;
+					}
+					else if ((row + 1) < 9) {
+						if (fillSudoku(puzzle, row + 1, 0))
+							return 1;
+						else
+							puzzle[row][col] = 0;
+					}
+					else
+						return 1;
+				}
+			}
+		}
+		return 0;
+	}
+	else
+		return 1;
 }
 
 int main() {
-    int i, j, testCase;
-    int puzzle[9][9];
+	int i, j, testCase;
+	int puzzle[9][9];
+	//LARGE_INTEGER start_time, finish_time, liFrequency;
 
-    freopen("input.txt", "r", stdin);
+	//QueryPerformanceFrequency(&liFrequency);
 
-    scanf("%d", &testCase);
+	freopen("input.txt", "r", stdin);
 
-    while (testCase--) {
-        for (i = 0; i < 9; i++) {
-            for (j = 0; j < 9; j++)
-                scanf("%d", &puzzle[i][j]);
-        }
+	scanf("%d", &testCase);
 
-        if (fillSudoku(puzzle, 0, 0)) {
-            for (i = 1; i < 10; ++i) {
-                for (j = 1; j < 10; ++j)
-                    printf("%d ", puzzle[i - 1][j - 1]);
-                printf("\n");
-            }
-        }
-        else
-            printf("NO SOLUTION");
-    }
+	while (testCase--) {
+		for (i = 0; i < 9; i++) {
+			for (j = 0; j < 9; j++)
+				scanf("%d", &puzzle[i][j]);
+		}
 
-    return 0;
+		//QueryPerformanceCounter(&start_time);
+		if (fillSudoku(puzzle, 0, 0)) {
+			for (i = 1; i < 10; ++i) {
+				for (j = 1; j < 10; ++j)
+					printf("%d ", puzzle[i - 1][j - 1]);
+				printf("\n");
+			}
+		}
+		else
+			printf("NO SOLUTION");
+		//QueryPerformanceCounter(&finish_time);
+
+		//printf("Time : %f\n", (double)(finish_time.QuadPart - start_time.QuadPart) / (double)liFrequency.QuadPart);
+	}
+
+	return 0;
 }
